554c99456d107a8ea49a9903805a4bbf98408539
Kris <k.koruev@kmail.bg>
2016-03-30 15:36:04 +0300

    Initial commit for algo course

---
+++ algo/heap/edu/heap/Heap.java
@@ -0,0 +1,26 @@
+package com.edu.heap;
+
+/**
+ * Trying to implement heap working only with integers and using array with fixed length
+ */
+
+public class Heap {
+    public static final Integer MAX_LEN = 10;
+    private Integer heapArray[];
+
+    public Heap(){
+        heapArray = new Integer[MAX_LEN];
+    }
+
+    public void insert(Integer value){
+
+    }
+
+    public void delete(Integer value){
+
+    }
+
+    public void update(Integer oldValue, Integer newValue){
+
+    }
+}
