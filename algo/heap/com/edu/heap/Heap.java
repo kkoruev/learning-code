11672506a888b2e20a8c525401cbad601e1aee7d
Kris <k.koruev@kmail.bg>
2016-03-30 19:43:45 +0300

    Added delete method in Heap

--- algo/heap/com/edu/heap/Heap.java
+++ algo/heap/com/edu/heap/Heap.java
@@ -6,6 +6,7 @@ package com.edu.heap;
 
 public class Heap {
     public static final int MAX_LEN = 10;
+
     private int len = 0;
     private Integer heapArray[];
 
@@ -18,11 +19,18 @@ public class Heap {
             throw new RuntimeException("Heap is full!");
         }
         heapArray[len] = value;
-        fixHeap(len);
+        fixHeapUp(len);
         len++;
     }
 
     public void delete(Integer value){
+        int valueIndex = findElementIndexForDeletion(value);
+        heapArray[valueIndex] = heapArray[len - 1];
+        len--;
+        fixHeapDown(valueIndex);
+    }
+
+    public void deleteRoot(){
 
     }
 
@@ -32,14 +40,16 @@ public class Heap {
 
     public String toString(){
         StringBuilder elements = new StringBuilder();
-        for(Integer element : heapArray){
+        int heapLen = len;
+        for(int i = 0; i < heapLen; i++){
+            Integer element = heapArray[i];
             elements.append(element);
             elements.append(",");
         }
         return new String(elements);
     }
 
-    private void fixHeap(int startIndex){
+    private void fixHeapUp(int startIndex){
         int indexCurrent = startIndex;
         int indexParent = (indexCurrent == 0) ? 0 : (indexCurrent - 1) / 2;
         while(heapArray[indexCurrent] > heapArray[indexParent]){
@@ -53,8 +63,39 @@ public class Heap {
         }
     }
 
+    private void fixHeapDown(int startIndex){
+        int indexCurrent = startIndex;
+        int childForSwapIndex = findChildForSwapIndex(indexCurrent);
+        while(heapArray[indexCurrent] < heapArray[childForSwapIndex]){
+            Integer temp = heapArray[indexCurrent];
+            heapArray[indexCurrent] = heapArray[childForSwapIndex];
+            heapArray[childForSwapIndex] = temp;
+
+            indexCurrent = childForSwapIndex;
+            childForSwapIndex = findChildForSwapIndex(indexCurrent);
+        }
+
+    }
+
     private boolean isFull(){
         return len >= MAX_LEN;
+    }
+
+    private int findChildForSwapIndex(int indexCurrent){
+        int firstChildIndex = (2 * indexCurrent + 1) > len ? indexCurrent : (2 * indexCurrent + 1);
+        int secondChildIndex = (2 * indexCurrent + 2) > len ? indexCurrent : (2 * indexCurrent + 2);
+        return (heapArray[firstChildIndex] > heapArray[secondChildIndex]) ? firstChildIndex : secondChildIndex;
+    }
+
+    private int findElementIndexForDeletion(Integer value){
+        int heapSize = len;
+        for(int i = 0; i < heapSize; i++){
+            Integer element = heapArray[i];
+            if(element.compareTo(value) == 0){
+                return i;
+            }
+        }
+        throw new RuntimeException("Element for deletion not found");
     }
 
 }
