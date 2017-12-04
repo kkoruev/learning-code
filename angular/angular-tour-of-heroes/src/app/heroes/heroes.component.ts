41b4ab5ceb2005dff943d1482a5ffd3a66c61ce8
Kris <k.koruev@abv.bg>
2017-12-05 00:06:36 +0200

    Added imports and html elemnts when needed in order project to compile and work

--- angular/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-heroes',
@@ -10,10 +12,10 @@ import { HEROES } from '../mock-heroes';
 export class HeroesComponent implements OnInit {
   hero: Hero = {
     id: 1,
-    name: 'Windstorm'
+    name: 'Windstorm' 
   };
 
-  heroes = HEROES;
+  heroes: Hero[]; 
 
   selectedHero: Hero;
   
@@ -21,8 +23,14 @@ export class HeroesComponent implements OnInit {
     this.selectedHero = hero;
   }
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes);
   }
 }
